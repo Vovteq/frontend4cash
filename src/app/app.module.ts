f5653add5253cbf7225322ce84b919fd1e05104d
kigris <kigris@ttu.ee>
2020-10-08 00:00:28 +0300

    Included bootstrap in the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { LinkButtonComponent } from './link-button/link-button.component';
+import { LinkButtonComponent } from './buttons/link-button/link-button.component';
 import {NgApexchartsModule} from "ng-apexcharts";
 import { ScriptBridgeComponent } from './script-bridge/script-bridge.component';
 import { ChartContainerComponent } from './chart-container/chart-container.component';
 import {HttpClientModule} from "@angular/common/http";
+import {BsDropdownModule} from "ngx-bootstrap/dropdown";
+import {TooltipModule} from "ngx-bootstrap/tooltip";
+import {ModalModule} from "ngx-bootstrap/modal";
 
 @NgModule({
   declarations: [
@@ -22,7 +25,10 @@ import {HttpClientModule} from "@angular/common/http";
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    NgApexchartsModule
+    NgApexchartsModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forChild()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/buttons/link-button/link-button.component.ts
+++ src/app/buttons/link-button/link-button.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import UrlClickContext from "../../scripts/ts/click-contexts/UrlClickContext";
+import UrlClickContext from "../../../scripts/ts/click-contexts/UrlClickContext";
 
 type urlCtx = UrlClickContext;
 
