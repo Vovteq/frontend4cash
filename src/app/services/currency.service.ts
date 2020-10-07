1b27220163b1202ab06bd74a6124154a7111800e
kigris <kigris@ttu.ee>
2020-10-07 23:14:01 +0300

    Added currency service and routing skeleton

---
+++ src/app/services/currency.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import Currency from "../../scripts/ts/currency/Currency";
+
+@Injectable()
+export class CurrencyService {
+  private readonly currenciesUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.currenciesUrl = 'http://backend4cash:8080/currency'
+  }
+
+  public getAllCurrencies(): Observable<Currency[]> {
+    return this.http.get<Currency[]>(this.currenciesUrl);
+  }
+
+  public saveCurrency(currency: Currency): void {
+    this.http.post<Currency>(this.currenciesUrl, currency);
+  }
+}
