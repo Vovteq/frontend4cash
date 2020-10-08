c45d631d3b196b1401f686d42662cdf4759668a9
gleb <glkomi@ttu.ee>
2020-10-08 22:07:47 +0300

    actually fixing frontend bugs (wrong port)

--- src/app/services/currency.service.ts
+++ src/app/services/currency.service.ts
@@ -8,7 +8,7 @@ export class CurrencyService {
   private readonly currenciesUrl: string;
 
   constructor(private http: HttpClient) {
-    this.currenciesUrl = 'http://backend4cash_dev:8069/api/coins/gleb'
+    this.currenciesUrl = 'http://backend4cash_dev:8080/api/coins/gleb'
   }
 
   public getAllCurrencies(): Observable<Currency[]> {
