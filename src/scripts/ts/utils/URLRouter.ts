4b61f123973aac09fe3c7b1d815fd0641cccbc57
kigris <kigris@ttu.ee>
2020-12-22 20:57:55 +0200

    https -> http

--- src/scripts/ts/utils/URLRouter.ts
+++ src/scripts/ts/utils/URLRouter.ts
@@ -2,7 +2,7 @@ import {Dictionary} from "../data-handling/Dictionary";
 import {isDevMode} from "@angular/core";
 
 export default class URLRouter {
-  public static readonly baseUrl = "https://localhost:8069/"
+  public static readonly baseUrl = "http://localhost:8069/"
   public static readonly connectionUrl = URLRouter.baseUrl + "api/coins/";
 
   private static routes: {[id: string]: string} = {
