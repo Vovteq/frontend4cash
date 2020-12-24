95c4a1e23df92743e2e47bca9eedaa7dcf40eb26
Gleb Komissarov <glkomi@ttu.ee>
2020-12-24 17:16:51 +0200

    back to what we had b4

--- src/scripts/ts/utils/URLRouter.ts
+++ src/scripts/ts/utils/URLRouter.ts
@@ -2,7 +2,7 @@ import {Dictionary} from "../data-handling/Dictionary";
 import {isDevMode} from "@angular/core";
 
 export default class URLRouter {
-  public static readonly baseUrl = "https://backend4cashdev:8443/"
+  public static readonly baseUrl = "http://backend4cashdev:8080/"
   public static readonly connectionUrl = URLRouter.baseUrl + "api/coins/";
 
   private static routes: {[id: string]: string} = {
