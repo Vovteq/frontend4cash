6f9d737e595fb4f1ac00807876b31fe607e3c3d0
kigris <kigris@ttu.ee>
2020-12-26 20:38:00 +0200

    Added proxy, fixed CORS errors

--- src/scripts/ts/utils/URLRouter.ts
+++ src/scripts/ts/utils/URLRouter.ts
@@ -2,8 +2,8 @@ import {Dictionary} from "../data-handling/Dictionary";
 import {isDevMode} from "@angular/core";
 
 export default class URLRouter {
-  public static readonly baseUrl = "http://193.40.156.55/"
-  public static readonly connectionUrl = URLRouter.baseUrl + "api/coins/";
+  public static readonly baseUrl = "";
+  public static readonly connectionUrl = URLRouter.baseUrl + "api/coins";
 
   private static routes: {[id: string]: string} = {
     currency: 'api/coins/',
